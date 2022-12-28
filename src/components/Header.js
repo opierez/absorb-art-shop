80eb577462b5bd03c2d3f036dde5b4e77b2f9b34
olivia <opierez@gmail.com>
2022-12-28 12:31:44 -0500

    header foundation and corresponding style sheets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from 'react';
+import '../styles/Header.css';
+import mercury_and_psyche from '../style-images/mercury_and_psyche.jpeg'
+import {BsChevronCompactDown} from 'react-icons/bs'
 function Header() {
     return (
-        <div></div>
+       <section id='header'>
+            <div className='header-container'>
+               <div className='header-left'>
+                <h1>
+                    <span>Company Name</span>
+                    <span>Tagline</span>
+                </h1>
+                <p>Mission Statement</p>
+                <div className='header-btn'>
+                    <a href='#'>Support Local Artists</a>
+                    <a href='#'>Submit Your Artwork</a>
+                </div>
+               </div>
+               <div className='header-right'>
+                    <img src={mercury_and_psyche} />
+               </div>
+            </div>
+            <div className='down-arrow'>
+                <a href='#'>
+                    <BsChevronCompactDown size={35} className='downbtn'/>
+                </a>
+            </div>
+       </section>
     )
 }
-
 export default Header
