e371b12f0d4642a781988eece1edb27040884fb2
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-01-03 10:02:25 -0600

    form setup

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Form() {
+    const [artist, setArtist] = useState("")
+    const [image, setImage] = useState("")
+    const [title, setTitle] = useState("")
+    const [product, setProduct] = useState("")
+    const [price, setPrice] = useState("")
+    const [description, setDescription] = useState("")
+    const [medium, setMedium] = useState("")
     return (
         <div>
             <h1>This is a Form</h1>
+            <form>
+                <input type="text" id="about" onChange={handleOnChange} />
+            </form>
         </div>
     )
 }
