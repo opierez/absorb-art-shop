d55d57f32280ae3d1c344ed6e2bc5bb905b2def6
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-01-04 11:04:45 -0600

    dimensions descriptor added to db.json

--- src/components/ArtCard.js
+++ src/components/ArtCard.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import '../styles/Cards.css'
-import ArtDetail from './ArtDetail'
+import { useHistory } from "react-router-dom";
 
 function ArtCard({ artpiece }) {
 
     // console.log(artpiece)
 
-    const {artist, image, product, title, price} = artpiece
+    const {id, artist, image, product, title, price, dimensions} = artpiece
 
-    // console.log(artist)
-    // console.log(image)
+    const history = useHistory();
 
+    const handleClick = (id) => {
+        history.push(`/artwork/${id}`)
+    }
 
     return (
             <li className='card'>
                 <h2>{artist}</h2>
-                <img src={image} alt={title}/>
+                <img src={image} alt={title} onClick={() => handleClick(id)}/>
                 <h4>{title}</h4>
+                <p>{dimensions}</p>
                 <p>{product} • {`$${parseFloat(price).toFixed(2)}`} </p>
             </li>
     )
--- src/components/ArtDetail.js
+++ src/components/ArtDetail.js
@@ -1,38 +1,55 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
+import '../styles/ArtDetails.css'
 
-
-function ArtDetail() {
+function ArtDetail({handleAddToCart}) {
 
     const [artwork, setArtwork] = useState([])
     const [isLoaded, setIsLoaded] = useState(false)
+    const [inCart, setInCart] = useState(false)
 
     const { id } = useParams()
-
+    
     useEffect(() => {
         fetch(`http://localhost:6001/artwork/${id}`)
-            .then(resp => resp.json())
-            .then(artwork => {
-                setArtwork(artwork)
-                setIsLoaded(!isLoaded)
-            })
+        .then(resp => resp.json())
+        .then(artwork => {
+            setArtwork(artwork)
+            setIsLoaded(!isLoaded)
+        })
     }, [id])
-
+    
     if (!isLoaded) return <h2>Loading...</h2>
-
-    const { artist, image, product, title, price, description } = artwork
-
-    console.log(image)
+    
+    const { artist, image, product, title, price, description, dimensions } = artwork
+    
+    function handleCart(){
+        setInCart(false)
+        if (inCart === true){
+            handleAddToCart(artwork)
+        }
+    }
+    // console.log(image)
 
 
     return (
-        <div>
-            <h1>Art Detail</h1>
-            <h2>{artist}</h2>
-            <img src={image} alt={title}/>
-            <h3>{product}</h3>
-            <p>{price}</p>
-            <p>{description}</p>
+        <div className="container">
+            <div className="column-1">
+                <h1>{title} by {artist}</h1>
+                <br></br>
+                <img className="image" src={image.substring(1,)} alt={title}/>
+            </div>
+            <div className="column-2">
+                <ul>
+                    <li>{description}</li>
+                    <li>{dimensions}</li>
+                    <li>{product}</li> 
+                    <li>{`$${parseFloat(price).toFixed(2)}`}</li>
+                    <li>
+                        <button className="cart-button" onClick={handleCart}>{inCart ? "In Cart!" : "Add to Cart"}</button>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
