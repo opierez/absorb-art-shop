8f92a016f1faf7c605cca237054074e1f147027e
olivia <opierez@gmail.com>
2023-01-05 12:27:35 -0500

    initial gallery component rendering

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import ArtCard from './ArtCard'
+import '../styles/Gallery.css'
 
-function Gallery() {
+function Gallery({ myGallery, renderImage }) {
+
+    // console.log(myGallery)
+
+
+    const renderMyGallery = myGallery.map(artpiece => {
+        return (
+        <div>
+            <ArtCard key={artpiece.id} artpiece={artpiece} renderImage={renderImage}/>
+            <button>Edit</button>
+        </div>
+        )
+    })
+
     return (
         <div>
             <h1>Gallery</h1>
+            {renderMyGallery}
         </div>
     )
 }
